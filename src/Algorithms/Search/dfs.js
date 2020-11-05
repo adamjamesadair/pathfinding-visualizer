829451bbbad848d870c7558912e0d6d6cb17dab7
adamjamesadair <adam.james.adair@gmail.com>
2020-11-05 07:17:06 -0800

    Fixed bug when no path is found

--- src/Algorithms/Search/dfs.js
+++ src/Algorithms/Search/dfs.js
@@ -3,15 +3,15 @@ import { animateAlgorithm, getUnvisitedNeighbors, getNodesInShortestPathOrder, c
 function computeDFS(grid, currentNode, finishNode, visitedNodes) {
     visitedNodes.push(currentNode);
     currentNode.isVisited = true;
-    if(currentNode === finishNode) return visitedNodes;
+    if(currentNode === finishNode) return [visitedNodes, visitedNodes];
     for(let unvisitedNeighbor of getUnvisitedNeighbors(currentNode, grid).filter(neighbor => neighbor.type !== "wallNode")) {
         if(!unvisitedNeighbor.isVisited){
             unvisitedNeighbor.previousNode = currentNode;
-            var newVisitedNodes = computeDFS(grid, unvisitedNeighbor, finishNode, visitedNodes);
-            if(newVisitedNodes)return newVisitedNodes;
+            var newVisitedNodes = computeDFS(grid, unvisitedNeighbor, finishNode, visitedNodes)[0];
+            if(newVisitedNodes) return [newVisitedNodes, newVisitedNodes];
         }
     }
-    return null;
+    return [null, visitedNodes];
 }
 
 export function visualizeDFS(algoVisualizer, grid, startNodeCoords, finishNodeCoords) {
@@ -19,7 +19,7 @@ export function visualizeDFS(algoVisualizer, grid, startNodeCoords, finishNodeCo
     clearPath(algoVisualizer);
     const startNode = grid[startNodeCoords[0]][startNodeCoords[1]];
     const finishNode = grid[finishNodeCoords[0]][finishNodeCoords[1]];
-    const visitedNodesInOrder = computeDFS(grid, startNode, finishNode, []);
+    const visitedNodesInOrder = computeDFS(grid, startNode, finishNode, [])[1];
     const nodesInShortestPathOrder = getNodesInShortestPathOrder(grid[finishNodeCoords[0]][finishNodeCoords[1]]);
-    animateAlgorithm(algoVisualizer, visitedNodesInOrder, nodesInShortestPathOrder);
+    if(visitedNodesInOrder) animateAlgorithm(algoVisualizer, visitedNodesInOrder, nodesInShortestPathOrder); else algoVisualizer.setState({running: false});
 }
